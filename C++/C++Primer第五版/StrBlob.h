43ed5913a477c60e1643a6bc05cf5bcd4078e244
To-Re <541536515@qq.com>
2020-07-20 22:13:22 +0800

    添加第 12 章动态指针相关内容

---
+++ C++/C++Primer第五版/StrBlob.h
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class StrBlob {
+public:
+    typedef vector<string>::size_type size_type;
+    StrBlob();
+    StrBlob(initializer_list<string> il);
+    size_type size() const {return data->size();}
+    bool empty() const {return data->empty();}
+    void push_back(const string &t) {data->push_back(t);}
+    void pop_back();
+    string& front();
+    string& back();
+private:
+    shared_ptr<vector<string>> data;
+    void check(size_type i, const string &msg) const;
+};
